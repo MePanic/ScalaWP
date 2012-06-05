94784dd3b7bcd6ac6c24e0f4f752bdc8e2489abd
Fabi <fabian.wib@gmail.com>
2012-06-05 19:36:10 +0200

    aufg 7 1

--- src/test/Aufg7.scala
+++ src/test/Aufg7.scala
@@ -2,51 +2,68 @@ package test
 
 import scala.actors.Actor
 import scala.actors.Actor._
+import scala.actors.TIMEOUT
 
-object Aufg7 extends Actor {
+object Aufg7_1 extends App {
 
-  def main(args: Array[String]) {
-
-    // RealFunction ist ein Synonym für Double => Double
-    type RealFunction = Double => Double
-    // beispielsweise:
-    val dSquare: RealFunction = (x: Double) => x*x
-    val realFunctionEvaluator = actor {
-      // ihr Code ...
-    }
-
-    actor {
-
-      realFunctionEvaluator!dSquare
-      realFunctionEvaluator ! 5
-      realFunctionEvaluator ! 7L
-      realFunctionEvaluator ! 10F
-      // Fehlerhafte Argumente werden mit NaN beantwortet
-      realFunctionEvaluator ! "Hallo"
-      realFunctionEvaluator ! 15.0
-      // ’DONE steht für: Berechnung mit dieser Funktion beendet!
-//      realFunctionEvaluator!’DONE
-      realFunctionEvaluator ! { (x: Double) => 1.0 / x }
-      realFunctionEvaluator ! 5
-      realFunctionEvaluator ! 7L
-      realFunctionEvaluator ! 10F
-      // Fehlerhafte Argument
-      realFunctionEvaluator ! { (s: String) => "Hallo" + s }
-      realFunctionEvaluator ! 0.0
-//      realFunctionEvaluator!’DONE
-      // Unangenehm: falsche Funktion erwartet ...
-      realFunctionEvaluator ! { (s: String) => "Hallo" + s }
-      realFunctionEvaluator ! 1.0
-//      realFunctionEvaluator!’EXIT
+  class realFuncEval extends Actor {
+    var func = (a: Double) => 2.0 * a: Double
+    def act {
       loop {
-        reactWithin(100) {
-          case r: Double => println(r)
-          case Error(s) => println(s)
-          case TIMEOUT => println("ende"); exit
+        receive {
+//          case s: (String => String) => reply(Double.NaN)
+          case f: RealFunction => func = f
+          case 'DONE =>
+          case 'EXIT => exit
+          case d: Double => reply(func(d))
+          case i: Int => reply(func(i))
+          case l: Long => reply(func(l))
+          case f: Float => reply(func(f))
+          case _ => reply(Double.NaN)
         }
       }
     }
-
   }
+
+  // RealFunction ist ein Synonym für Double => Double
+  type RealFunction = Double => Double
+  // beispielsweise:
+  val dSquare: RealFunction = (x: Double) => x * x
+  val realFunctionEvaluator = new realFuncEval
+  realFunctionEvaluator.start
+
+  val acting = actor {
+
+    realFunctionEvaluator ! dSquare
+    realFunctionEvaluator ! 5
+    realFunctionEvaluator ! 7L
+    realFunctionEvaluator ! 10F
+    // Fehlerhafte Argumente werden mit NaN beantwortet
+    realFunctionEvaluator ! "Hallo"
+    realFunctionEvaluator ! 15.0
+    // ’DONE steht für: Berechnung mit dieser Funktion beendet!
+    // realFunctionEvaluator ! ’DONE
+    realFunctionEvaluator ! { (x: Double) => 1.0 / x }
+    realFunctionEvaluator ! 5
+    realFunctionEvaluator ! 7L
+    realFunctionEvaluator ! 10F
+    // Fehlerhafte Argument
+    //    realFunctionEvaluator ! { (s: String) => "Hallo" + s }
+    realFunctionEvaluator ! 0.0
+    // realFunctionEvaluator ! ’DONE
+    // Unangenehm: falsche Funktion erwartet ...
+    //    realFunctionEvaluator ! { (s: String) => "Hallo" + s }
+    realFunctionEvaluator ! 1.0
+    // realFunctionEvaluator ! ’EXIT
+    loop {
+      reactWithin(100) {
+        case r: Double => println(r)
+        case s: String => println(s)
+        case TIMEOUT => println("ende"); exit
+      }
+    }
+  }
+
+  acting.start
 
 }
