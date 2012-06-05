6c97cb1ceddb218341dd4a196967e4608ccb64ad
Fabi <fabian.wib@gmail.com>
2012-06-05 19:54:47 +0200

    aufg 7 2

--- src/test/Aufg7.scala
+++ src/test/Aufg7.scala
@@ -4,14 +4,14 @@ import scala.actors.Actor
 import scala.actors.Actor._
 import scala.actors.TIMEOUT
 
-object Aufg7_1 extends App {
+object Aufg7_1 {
 
   class realFuncEval extends Actor {
     var func = (a: Double) => 2.0 * a: Double
     def act {
       loop {
         receive {
-//          case s: (String => String) => reply(Double.NaN)
+          //          case s: (String => String) => reply(Double.NaN)
           case f: RealFunction => func = f
           case 'DONE =>
           case 'EXIT => exit
@@ -42,7 +42,7 @@ object Aufg7_1 extends App {
     realFunctionEvaluator ! "Hallo"
     realFunctionEvaluator ! 15.0
     // ’DONE steht für: Berechnung mit dieser Funktion beendet!
-    // realFunctionEvaluator ! ’DONE
+    realFunctionEvaluator ! 'DONE
     realFunctionEvaluator ! { (x: Double) => 1.0 / x }
     realFunctionEvaluator ! 5
     realFunctionEvaluator ! 7L
@@ -50,11 +50,11 @@ object Aufg7_1 extends App {
     // Fehlerhafte Argument
     //    realFunctionEvaluator ! { (s: String) => "Hallo" + s }
     realFunctionEvaluator ! 0.0
-    // realFunctionEvaluator ! ’DONE
+    realFunctionEvaluator ! 'DONE
     // Unangenehm: falsche Funktion erwartet ...
     //    realFunctionEvaluator ! { (s: String) => "Hallo" + s }
     realFunctionEvaluator ! 1.0
-    // realFunctionEvaluator ! ’EXIT
+    realFunctionEvaluator ! 'EXIT
     loop {
       reactWithin(100) {
         case r: Double => println(r)
@@ -66,4 +66,55 @@ object Aufg7_1 extends App {
 
   acting.start
 
+}
+
+object Aufg7_2 {
+
+  class pingPong extends Actor {
+    var ping = false
+    def act {
+      loop {
+        react {
+          case 'Ping => if (!ping) { ping = true; println("ping erhalten") }
+          case 'Pong => if (ping) { ping = false; println("pong erhalten") }
+          case 'EXIT => if (!ping) { println("ende"); exit }
+        }
+      }
+    }
+  }
+
+  //  lazy val pingPong = actor {
+  //    var ping = false
+  //    def act {
+  //      loop {
+  //        reactWithin(1000) {
+  //          case 'Ping => if (!ping) { ping = true; println("ping erhalten") }
+  //          case 'Pong => if (ping) { ping = false; println("pong erhalten") }
+  //          case 'Exit => if (!ping) { println("ende"); exit }
+  //          case TIMEOUT => { println("timeout"); exit }
+  //        }
+  //      }
+  //    }
+  //  }
+
+  lazy val pingPong = new pingPong
+  
+  pingPong.start
+
+  val acting = actor {
+    pingPong ! 'Ping
+    pingPong ! 1
+    pingPong ! 'Ping
+    pingPong ! 'Pong
+    pingPong ! 'Pong
+    pingPong ! "Hallo"
+    pingPong ! 'Ping
+    pingPong ! 'Ping
+    pingPong ! 'EXIT
+    pingPong ! 'Pong
+    pingPong ! 'EXIT
+    pingPong ! 'Ping
+  }
+
+  acting.start
 }
